76f52c11f774e675c3a872480badc07842df7e74
i-break-codes <vaibhav@browserstack.com>
2020-05-09 20:59:46 +0530

    add sentry monitoring

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueMeta from 'vue-meta';
 import VueClipboard from 'vue-clipboard2';
 import * as vClickOutside from 'v-click-outside-x';
+import { Vue as VueIntegration } from '@sentry/integrations';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import './registerServiceWorker';
 import router from './router';
@@ -17,6 +19,11 @@ Vue.use(VueMeta, {
 });
 
 Vue.use(VueClipboard);
+
+Sentry.init({
+  dsn: 'https://69c3e63fff53471faddbc91e38a70bfd@o390129.ingest.sentry.io/5231070',
+  integrations: [new VueIntegration({ Vue, attachProps: true })],
+});
 
 new Vue({
   router,
