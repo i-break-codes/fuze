a8e494b807334f9d05fa4e3753586141f798d193
i-break-codes <vaibhav@browserstack.com>
2020-05-09 21:23:30 +0530

    add google analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueClipboard from 'vue-clipboard2';
 import * as vClickOutside from 'v-click-outside-x';
 import { Vue as VueIntegration } from '@sentry/integrations';
 import * as Sentry from '@sentry/browser';
+import VueAnalytics from 'vue-analytics';
 import App from './App';
 import './registerServiceWorker';
 import router from './router';
@@ -24,6 +25,13 @@ Sentry.init({
   dsn: 'https://69c3e63fff53471faddbc91e38a70bfd@o390129.ingest.sentry.io/5231070',
   integrations: [new VueIntegration({ Vue, attachProps: true })],
 });
+
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-166017740-1',
+    router,
+  });
+}
 
 new Vue({
   router,
